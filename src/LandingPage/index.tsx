9334f29fde44b1c07d54cad48b4d81e53b16c47c
David Knittel <djknit@gmail.com>
2023-04-06 22:38:16 -0500

    added router and started directory structure planning and implementation

---
+++ src/LandingPage/index.tsx
@@ -0,0 +1,10 @@
+
+function LandingPage({}) {
+    return (
+        <div id="landing-pg">
+            <h1>WELCOME!</h1>
+        </div>
+    );
+}
+
+export default LandingPage;
---
+++ src/MainApp/index.tsx
@@ -0,0 +1,11 @@
+
+
+function MainApp({}) {
+    return (
+        <div id="main-app">
+            <h1>MAIN APP</h1>
+        </div>
+    );
+}
+
+export default MainApp;
